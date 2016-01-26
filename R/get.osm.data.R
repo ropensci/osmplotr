d725247b832f5fe0ed5e531767f5019a18d182ef
mpadge <mark.padgham@email.com>
2016-01-26 22:23:57 +0100

    change mapquest to overpass api
    add make.query.R and make corresponding changes to get.osm.data.R

--- R/get.osm.data.R
+++ R/get.osm.data.R
@@ -2,14 +2,15 @@
 #'
 #' Downloads OSM data for selected object type from the MapQuest api.
 #'
-#' @param type: currently recognises "building", "waterway", "natural", "grass",
-#' "park", "amenity", "shop", "boundary", and "highway". Others will be simply
-#' passed to "way[type=*]", which may or may not work.
+#' @param key: OSM key to search for. (Useful keys include "building",
+#' "waterway", "natural", "grass", "park", "amenity", "shop", "boundary", and
+#' "highway". Others will be simply passed to "way[type=*]", which may or may
+#' not work.)
 #' @param bbox = the bounding box within which all objects of the given type
 #' should be downloaded. Default is central London (-0.15,51.5,-0.1,51.52).
 #' @return The contents of
 
-get.osm.data <- function (type="building", bbox=c(-0.15,51.5,-0.1,51.52))
+get.osm.data <- function (key="building", value=NULL, bbox=c(-0.15,51.5,-0.1,51.52))
 {
     stopifnot (is.numeric (bbox))
     stopifnot (length (bbox) == 4)
@@ -17,42 +18,24 @@ get.osm.data <- function (type="building", bbox=c(-0.15,51.5,-0.1,51.52))
         bbox <- bbox [c (3, 2, 1, 4)]
     if (bbox [4] <- bbox [2])
         bbox <- bbox [c (1, 4, 3, 2)]
-    bbox <- paste ("[bbox=", bbox [1], ",", bbox [2], ",",
-                   bbox[3], ",", bbox [4], "]", sep="")
 
-    url.base <- "http://open.mapquestapi.com/xapi/api/0.6/"
-    if (type == "building") 
+    if (key == "water")
     {
-        dat <- getURL (paste (url.base, "way[building=*]", bbox, sep=""))
-    } else if (type == "waterway") 
+        key <- "natural"
+        value <- "water"
+    } else if (key == "grass")
     {
-        dat <- getURL (paste (url.base, "way[waterway=*]", bbox, sep=""))
-    } else if (type == "natural") 
+        key <- "landuse"
+        value <- "grass"
+    } else if (key == "park")
     {
-        dat <- getURL (paste (url.base, "way[natural=water]", bbox, sep=""))
-    } else if (type == "grass") 
-    {
-        dat <- getURL (paste (url.base, "way[landuse=grass]", bbox, sep=""))
-    } else if (type == "park") 
-    {
-        dat <- getURL (paste (url.base, "way[leisure=park]", bbox, sep=""))
-    } else if (type == "amenity") 
-    {
-        dat <- getURL (paste (url.base, "way[amenity=*]", bbox, sep=""))
-    } else if (type == "shop") 
-    {
-        dat <- getURL (paste (url.base, "way[shop=*]", bbox, sep=""))
-    } else if (type == "boundary") 
-    {
-        dat <- getURL (paste (url.base, "way[boundary=*]", bbox, sep=""))
-    } else if (type == "highway") 
-    {
-        dat <- getURL (paste (url.base, "way[highway=*]", bbox, sep=""))
-    } else 
-    {
-        url.str <- paste (url.base, "way[", type, "=*]", bbox, sep="")
-        try ( dat <- getURL (url.str) )
+        key <- "leisure"
+        value <- "park"
     }
+    
+    url.base <- 'http://overpass-api.de/api/interpreter?data='
+    dat <- RCurl::getURL (make.query (bbox, key=key, value=value))
+    dat <- XML::xmlParseString (dat)
 
     if (is.null (dat))
         warning ("No data downloaded.")
