6d6d8ba48526bfc5edd98f64bcfbf3d19aa78eb4
mpadge <mark.padgham@email.com>
2016-02-06 00:43:59 +0100

    update R/urbanplotr.R

--- R/urbanplotr.R
+++ R/urbanplotr.R
@@ -5,6 +5,8 @@
 #' @import RCurl sp spatstat spatialkernel osmar XML igraph ggm
 NULL
 
+#' datA OSM polygons for amenities in central London
+#'
 #' A SpatialPolygonsDataFrame containing OpenStreetMap polygons for amenities
 #' (key=amenity) in a small part of central London, U.K.
 #' (bbox = -0.15, 51.5, -0.1, 51.52).
@@ -16,6 +18,8 @@ NULL
 #' @format A SpatialPolygonsDataFrame with 1,157 polygons
 NULL
 
+#' datBU OSM polygons for buildings in central London
+#'
 #' A SpatialPolygonsDataFrame containing OpenStreetMap polygons for buildings
 #' (key=building) in a small part of central London, U.K.
 #' (bbox = -0.15, 51.5, -0.1, 51.52).
@@ -27,6 +31,8 @@ NULL
 #' @format A SpatialPolygonsDataFrame with 6,209 polygons
 NULL
 
+#' datG OSM polygons for grassed areas in central London
+#'
 #' A SpatialPolygonsDataFrame containing OpenStreetMap polygons for grassed
 #' areas (key=landuse, value=grass) in a small part of central London, U.K.
 #' (bbox = -0.15, 51.5, -0.1, 51.52).
@@ -38,6 +44,8 @@ NULL
 #' @format A SpatialPolygonsDataFrame with 50 polygons
 NULL
 
+#' datH Lines for OSM highways in central London
+#'
 #' A SpatialLinesDataFrame containing OpenStreetMap lines for all highways
 #' (key=highway) in a small part of central London, U.K.
 #' (bbox = -0.15, 51.5, -0.1, 51.52).
@@ -49,6 +57,8 @@ NULL
 #' @format A SpatialLinesDataFrame with 4,517 lines 
 NULL
 
+#' datN OSM polygons for natural areas in central London
+#'
 #' A SpatialPolygonsDataFrame containing OpenStreetMap polygons for natural
 #' areas (key=natural) in a small part of central London, U.K.
 #' (bbox = -0.15, 51.5, -0.1, 51.52).
@@ -60,6 +70,8 @@ NULL
 #' @format A SpatialPolygonsDataFrame with 36 polygons
 NULL
 
+#' datP OSM polygons for parks in central London
+#'
 #' A SpatialPolygonsDataFrame containing OpenStreetMap polygons for parks
 #' (key=leisure, value=park) in a small part of central London, U.K.
 #' (bbox = -0.15, 51.5, -0.1, 51.52).
