0fb56ab19935fdc0f4bd3a4e245e93b6b2a29346
mpadge <mark.padgham@email.com>
2016-02-17 14:03:42 +0100

    update data/london

--- R/osmplotr.R
+++ R/osmplotr.R
@@ -24,9 +24,42 @@ NULL
 #'  \item dat_ST an SPDF containing 1 polygon representing 150 Stamford Street
 #' }
 #'
+#' @examples
+#' \dontrun{
+#' # Data were downloaded using the following code:
+#' structures <- c ("highway", "highway", "building", "building", "building",
+#'                  "amenity", "grass", "park", "natural", "tree")   
+#' structs <- osm_structures (structures=structures, col_scheme="dark")   
+#' structs$value [1] <- "!primary"   
+#' structs$value [2] <- "primary"
+#' structs$suffix [2] <- "HP"
+#' structs$value [3] <- "!residential"
+#' structs$value [4] <- "residential"
+#' structs$value [5] <- "commercial"
+#' structs$suffix [3] <- "BNR"
+#' structs$suffix [4] <- "BR"
+#' structs$suffix [5] <- "BC"
+#' london <- list ()
+#' for (i in 1:(nrow (structs) - 1)) {
+#'     dat <- extract_osm_objects (key=structs$key [i], value=structs$value [i],
+#'                                bbox=bbox)
+#'     fname <- paste0 ("dat_", structs$suffix [i])
+#'     assign (fname, dat)
+#'     london [[i]] <- get (fname)
+#'     names (london)[i] <- fname
+#'     rm (list=c(fname))
+#' }
+#' extra_pairs <- c ("name", "Royal.Festival.Hall")
+#' london$dat_RFH <- extract_osm_objects (key="building", extra_pairs=extra_pairs, 
+#'                                 bbox=bbox)
+#' extra_pairs <- list (c ("addr:street", "Stamford.St"),
+#'                      c ("addr:housenumber", "150"))
+#' london$dat_ST <- extract_osm_objects (key="building", extra_pairs=extra_pairs, 
+#'                                 bbox=bbox)
+#' }
+#'
 #' @docType data
 #' @keywords datasets
 #' @name london
-#' @usage data(london)
 #' @format A list of spatial objects
 NULL
