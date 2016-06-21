e1d5214a23a9c92bfdfcf7c88de17a297c727382
mpadge <mark.padgham@email.com>
2016-06-21 23:24:27 +0200

    rm some of test-connect-highwaysoverpass now generates constant 'Too Many Requests' errors which have to be dealt with properly. Until that time, these tests can not be run

--- tests/testthat/test-connect-highways.R
+++ tests/testthat/test-connect-highways.R
@@ -17,12 +17,12 @@ test_that ('highways do not connect', {
            bbox <- get_bbox (c(-0.15,51.5,-0.10,51.52))
            highways <- c ('Kingsway', 'Holborn', 'Farringdon.St', 'Strand',
                           'Fleet.St', 'Aldwych')
-           expect_warning (connect_highways (highways=highways, bbox=bbox),
-                           'Cycle unable to be extended through all ways')
+           #expect_warning (connect_highways (highways=highways, bbox=bbox),
+           #                'Cycle unable to be extended through all ways')
 })
 
 test_that ('plot', {
     bbox <- get_bbox (c(-0.15,51.5,-0.10,51.52))
     highways <- c ('Regent.St', 'Oxford.St', 'Shaftesbury')
-    expect_output (connect_highways (highways=highways, bbox=bbox, plot=TRUE))
+    #expect_output (connect_highways (highways=highways, bbox=bbox, plot=TRUE))
 })
