eae3d05a4766beedbd3818c022baf5a3e30f89d7
mpadge <mark.padgham@email.com>
2016-07-04 22:27:08 +0200

    update test-extract-objects

--- tests/testthat/test-extract-objects.R
+++ tests/testthat/test-extract-objects.R
@@ -5,14 +5,21 @@ test_that ('missing objects', {
            expect_error (extract_osm_objects ('aaa'), 'bbox must be provided')
 })
 
-test_that ('key missing or junk', {
+test_that ('key missing', {
            bbox <- get_bbox (c (-0.12, 51.51, -0.11, 51.52))
            expect_error (extract_osm_objects (bbox=bbox, key=''), 
                          'key must be provided')
            expect_error (extract_osm_objects (bbox=bbox), 'key must be provided')
+})
+
+test_that ('invalid key', {
+           bbox <- get_bbox (c (-0.12, 51.51, -0.11, 51.52))
            expect_warning (dat <- extract_osm_objects (bbox=bbox, key='aaa'),
                            'No valid data for')
-           expect_null (dat)
+})
+
+test_that ('valid key', {
+           bbox <- get_bbox (c (-0.12, 51.51, -0.11, 51.52))
            expect_silent (dat <- extract_osm_objects (bbox=bbox,
                                                       key='building'))
            expect_is (dat, 'SpatialPolygonsDataFrame')
