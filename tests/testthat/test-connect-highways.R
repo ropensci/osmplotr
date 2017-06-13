8e4f2d91f66d347bdc5bce09f53bc09b92555f2b
mpadge <mark.padgham@email.com>
2017-06-13 13:58:54 +0200

    test everything except on travis, appveyor

--- tests/testthat/test-connect-highways.R
+++ tests/testthat/test-connect-highways.R
@@ -1,5 +1,10 @@
 context ('connect-highways')
 
+# test_all used to switch off tests on CRAN
+test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
+             identical (Sys.getenv ("TRAVIS"), "true") |
+             identical (Sys.getenv ("APPVEYOR"), "True"))
+
 if (curl::has_internet ()) # otherwise all of these return errors not warnings
 {
     test_that ('missing objects', {
@@ -9,40 +14,33 @@ if (curl::has_internet ()) # otherwise all of these return errors not warnings
                              'A bounding box must be given')
     })
 
-    test_that ('unrecognised highways', {
-               skip_on_cran ()
+    if (test_all)
+    {
+        test_that ('unrecognised highways', {
+                       bbox <- get_bbox (c (-0.13, 51.5, -0.11, 51.52))
+                       # No error specified because different HTML errors may
+                       # also be generated
+                       expect_error (connect_highways ('aaa', bbox))
+                       #expect_error (connect_highways ('aaa', bbox),
+                       #              'No data able to be extracted')
+        })
 
-               bbox <- get_bbox (c (-0.13, 51.5, -0.11, 51.52))
-               # No error specified because different HTML errors may also be
-               # generated
-               expect_error (connect_highways ('aaa', bbox))
-               #expect_error (connect_highways ('aaa', bbox),
-               #              'No data able to be extracted')
-    })
+        test_that ('highways do not connect', {
+                   bbox <- get_bbox (c(-0.15, 51.5, -0.10, 51.52))
+                   highways <- c ('Kingsway', 'Holborn', 'Farringdon.St',
+                                  'Strand', 'Fleet.St', 'Aldwych')
+                   # Again, warnings will differ if download fails
+                   expect_warning (connect_highways (highways = highways,
+                                                     bbox = bbox))
+        })
 
-    test_that ('highways do not connect', {
-               skip_on_cran ()
-
-               bbox <- get_bbox (c(-0.15, 51.5, -0.10, 51.52))
-               highways <- c ('Kingsway', 'Holborn', 'Farringdon.St', 'Strand',
-                              'Fleet.St', 'Aldwych')
-               # No warning specified here because different warnings will be
-               # given if download fails
-               expect_warning (connect_highways (highways = highways,
-                                                 bbox = bbox))
-               #expect_warning (connect_highways (highways = highways, bbox =
-               #                                  bbox),
-               #                'Cycle unable to be extended through all ways')
-    })
-
-    test_that ('plot', {
-               skip_on_cran ()
-
-               bbox <- get_bbox (c(-0.15, 51.5, -0.10, 51.52))
-               highways <- c ('Regent.St', 'Oxford.St', 'Shaftesbury')
-               # This can't fail because failed downloads will generate warnings
-               # and successful ones will generate a plot.
-               expect_output (connect_highways (highways = highways,
-                                                bbox = bbox, plot = TRUE))
-    })
+        test_that ('plot', {
+                   bbox <- get_bbox (c(-0.15, 51.5, -0.10, 51.52))
+                   highways <- c ('Regent.St', 'Oxford.St', 'Shaftesbury')
+                   # This can't fail because failed downloads will generate warnings
+                   # and successful ones will generate a plot.
+                   expect_output (connect_highways (highways = highways,
+                                                    bbox = bbox, plot = TRUE))
+        })
+    } # end if (test_all)
 } # end if has_internet
--- tests/testthat/test-extract-objects.R
+++ tests/testthat/test-extract-objects.R
@@ -1,5 +1,10 @@
 context ('extract-objects')
 
+# test_all used to switch off tests on CRAN
+test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
+             identical (Sys.getenv ("TRAVIS"), "true") |
+             identical (Sys.getenv ("APPVEYOR"), "True"))
+
 test_that ('missing objects', {
            expect_error (extract_osm_objects (), 'key must be provided')
            expect_error (extract_osm_objects ('aaa'), 'bbox must be provided')
@@ -13,22 +18,16 @@ test_that ('key missing', {
                          'key must be provided')
     })
 
-if (curl::has_internet ()) # otherwise all of these return errors not warnings
+if (curl::has_internet () & test_all)
 {
     test_that ('invalid key', {
-               skip_on_cran ()
-
                bbox <- get_bbox (c (-0.12, 51.51, -0.11, 51.52))
-               # No warning specified here because different warnings will be
-               # given if download fails
                expect_warning (dat <- extract_osm_objects (bbox = bbox,
                                                            key = 'aaa'))
                expect_null (dat)
     })
 
     test_that ('valid key', {
-               skip_on_cran ()
-
                bbox <- get_bbox (c (-0.12, 51.515, -0.115, 51.52))
                dat <- extract_osm_objects (bbox = bbox, key = 'building')
                # Will return NULL if any warnings are generated (such as by
@@ -38,19 +37,15 @@ if (curl::has_internet ()) # otherwise all of these return errors not warnings
     })
 
     test_that ('return_type', {
-               skip_on_cran ()
-
                key <- 'route'
                value <- 'bicycle'
                extra_pairs <- c ('name', 'London.Cycle.Network.Route.9')
                bbox <- get_bbox (c (-0.26, 51.47, 0.11, 51.67))
-               # This can't be run because any downloading issues generate
-               # warnings. Only if downloading succeeds will the message be
-               # generated ('Cannot determine return_type, maybe specify
-               # explicitly?')
+               # NOTE: any downloading issues will generate warnings here. Only
+               # if downloading succeeds will the message be generated ('Cannot
+               # determine return_type, maybe specify explicitly?')
                expect_message (dat <- extract_osm_objects (bbox = bbox,
                                         key = key, value = value,
                                         extra_pairs = extra_pairs))
     })
-
 } # end if has_internet
