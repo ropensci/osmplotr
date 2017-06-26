a560cfa13a4a1cba5c468eb075086c59aac7e687
mpadge <mark.padgham@email.com>
2017-06-26 22:21:46 +0200

    temp explicit file types for test-print-map
    travis osx fails on one of these, and this is only intended to locate which

--- tests/testthat/test-print-map.R
+++ tests/testthat/test-print-map.R
@@ -11,17 +11,47 @@ test_that ('print_osm_map', {
 
                exts <- c ('eps', 'ps', 'tex', 'pdf', 'svg', 'png',
                           'jpg', 'jpeg', 'bmp', 'tiff')
-               for (e in exts)
-               {
-                   fname <- paste0 ('map.', e)
-                   expect_silent (print_osm_map (map, width = 5, height = 4,
-                                                 filename = fname,
-                                                 units = "in"))
-                   expect_true (fname %in% list.files ())
-               }
-               if (test_all)
-               {
-                   for (e in exts)
-                       file.remove (paste0 ('map.', e))
-               }
+               #for (e in exts)
+               #{
+               #    fname <- paste0 ('map.', e)
+               #    expect_silent (print_osm_map (map, width = 5, height = 4,
+               #                                  filename = fname,
+               #                                  units = "in"))
+               #    expect_true (fname %in% list.files ())
+               #}
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.eps",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.ps",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.tex",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.pdf",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.svg",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.png",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.jpg",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.jpeg",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.bmp",
+                                             units = "in"))
+               expect_silent (print_osm_map (map, width = 5, height = 4,
+                                             filename = "map.tiff",
+                                             units = "in"))
+               #if (test_all)
+               #{
+               #    for (e in exts)
+               #        file.remove (paste0 ('map.', e))
+               #}
 })
