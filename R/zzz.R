deb32dac4e351acae5298143b9dd379a229d6f76
mpadge <mark.padgham@email.com>
2019-02-05 15:23:24 +0100

    closes #46

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 .onAttach <- function(libname, pkgname) {
     msg <- paste0 ('Data (c) OpenStreetMap contributors, ',
                    'ODbL 1.0. http://www.openstreetmap.org/copyright')
+    if (!requireNamespace ("sf"))
+        msg <- paste0 (msg, "\nNOTE: 'sf' package must be loaded for 'sf' methods ",
+                       "to work")
     packageStartupMessage(msg)
 }
