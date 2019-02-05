53896532f35741eff40e6501b297d4ce76a2cbbf
mpadge <mark.padgham@email.com>
2019-02-05 23:13:53 +0100

    requireNamespace msg in onLoad not onAttach; thanks @dmurdoch

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,12 @@
 .onAttach <- function(libname, pkgname) {
     msg <- paste0 ('Data (c) OpenStreetMap contributors, ',
                    'ODbL 1.0. http://www.openstreetmap.org/copyright')
+    packageStartupMessage(msg)
+}
+
+.onLoad <- function(libname, pkgname) {
     if (!requireNamespace ("sf"))
-        msg <- paste0 (msg, "\nNOTE: 'sf' package must be loaded for 'sf' methods ",
+        msg <- paste0 ("NOTE: 'sf' package must be loaded for 'sf' methods ",
                        "to work")
     packageStartupMessage(msg)
 }
