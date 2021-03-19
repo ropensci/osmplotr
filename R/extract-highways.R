0d6f36ca1e0f26a30c83f9b78d1ed8df1d506e98
mpadge <mark.padgham@email.com>
2021-03-19 10:38:14 +0100

    line extract-highways.R

--- R/extract-highways.R
+++ R/extract-highways.R
@@ -10,25 +10,25 @@
 #' Overpass API. Wildcards and whitespaces are `.'; for other options see
 #' overpass help.
 #' @param bbox the bounding box for the map.  A 2-by-2 matrix of 4 elements with
-#' columns of min and max values, and rows of x and y values.  
+#' columns of min and max values, and rows of x and y values.
 #' @return A list of highways matching \code{highway_names} each element of
 #' which is a list of distinct components for the given highway.
 #' @return A \code{data.frame} of \code{sp} objects
 #'
 #' @noRd
-extract_highways <- function (highway_names, bbox)
-{
+extract_highways <- function (highway_names, bbox) {
+
     if (missing (highway_names))
-        stop ('A vector of highway names must be given')
+        stop ("A vector of highway names must be given")
     if (missing (bbox))
-        stop ('A bounding box must be given')
+        stop ("A bounding box must be given")
 
     #----------Download OSM data for highways
     hw_abbrvs <- abbreviate_hwy_names (highway_names)
     dl_dat <- dl_hw_data (highway_names, hw_abbrvs, bbox)
     p4s <- dl_dat$p4s
-    if (length (dl_dat$indx) < length (highway_names))
-    {
+    if (length (dl_dat$indx) < length (highway_names)) {
+
         hw_abbrvs <- hw_abbrvs [dl_dat$indx]
         highway_names <- highway_names [dl_dat$indx]
     }
@@ -36,8 +36,8 @@ extract_highways <- function (highway_names, bbox)
 
     #---------Extract coordinates
     ways <- list ()
-    for (i in seq (highway_names))
-    {
+    for (i in seq (highway_names)) {
+
         wi <- lapply (get (hw_abbrvs [i])$geometry,
                       function (i) as.matrix (i))
         ways [[i]] <- order_lines (wi)
@@ -49,12 +49,12 @@ extract_highways <- function (highway_names, bbox)
     return (ways)
 }
 
-abbreviate_hwy_names <- function (highway_names, nletters = 2)
-{
+abbreviate_hwy_names <- function (highway_names, nletters = 2) {
+
     hw_abbrvs <- sapply (highway_names, function (x)
                       tolower (substring (x, 1, nletters)))
-    while (any (duplicated (hw_abbrvs)))
-    {
+    while (any (duplicated (hw_abbrvs))) {
+
         nletters <- nletters + 1
         hw_abbrvs <- sapply (highway_names, function (x)
                           tolower (substring (x, 1, nletters)))
@@ -63,25 +63,25 @@ abbreviate_hwy_names <- function (highway_names, nletters = 2)
     return (hw_abbrvs)
 }
 
-dl_hw_data <- function (highway_names, hw_abbrvs, bbox)
-{
-    cat ('Downloading OSM data ...\n')
+dl_hw_data <- function (highway_names, hw_abbrvs, bbox) {
+
+    cat ("Downloading OSM data ...\n")
     p4s <- NULL
     lens_old <- length (highway_names)
     lens <- 0
     # in case download does not work, this will try again until same data are
     # returned twice in a row
-    while (lens != lens_old)
-    {
+    while (lens != lens_old) {
+
         indx <- NULL
         pb <- txtProgressBar (max = 1, style = 3)
         # style = 3 shows start and end positions
-        for (i in seq (highway_names))
-        {
+        for (i in seq (highway_names)) {
+
             dat <- extract_highway (name = highway_names [i], bbox = bbox)
             if (!is.null (dat))
-                if (nrow (dat) > 0)
-                {
+                if (nrow (dat) > 0) {
+
                     assign (hw_abbrvs [i], value = dat, envir = parent.frame ())
                     indx <- c (indx, i)
                 }
@@ -93,22 +93,22 @@ dl_hw_data <- function (highway_names, hw_abbrvs, bbox)
                                    envir = parent.frame ()))
 
         lens <- length (which (lens > 0)) # total number returning data
-        if (lens > 0)
-        {
+        if (lens > 0) {
+
             hw1 <- hw_abbrvs [indx] [which (lens > 0)] [1]
             p4s <- attr (get (hw1, envir = parent.frame ())$geometry,
-                         'crs')$proj4string
+                         "crs")$proj4string
         }
         rm (dat)
         close (pb)
         lens_old <- lens
     }
     if (lens == 0)
-        stop ('No data able to be extracted')
+        stop ("No data able to be extracted")
     else if (lens < length (highway_names))
-        message ('Unable to download all requested data.')
+        message ("Unable to download all requested data.")
 
-    list ('p4s' = p4s, 'indx' = indx)
+    list ("p4s" = p4s, "indx" = indx)
 }
 
 
@@ -118,20 +118,20 @@ dl_hw_data <- function (highway_names, hw_abbrvs, bbox)
 #'
 #' @param name Name of highway. Lines components are return for *any* OSM way
 #' with a partially-matched. Both wildcards and whitespace should be represented
-#' by `.'. 
+#' by `.'.
 #' @param bbox the bounding box for the map.  A 2-by-2 matrix of 4 elements with
-#' columns of min and max values, and rows of x and y values.  
+#' columns of min and max values, and rows of x and y values.
 #' @return A \code{SpatialLinesDataFrame} containing the highway.
 #'
 #' @noRd
-extract_highway <- function (name = '', bbox)
-{
-    check_arg (name, 'name', 'character')
+extract_highway <- function (name = "", bbox) {
+
+    check_arg (name, "name", "character")
     bbox <- check_bbox_arg (bbox)
 
     qry <- osmdata::opq (bbox = bbox)
-    qry <- osmdata::add_osm_feature (qry, key = 'highway')
-    qry <- osmdata::add_osm_feature (qry, key = 'name', value = name,
+    qry <- osmdata::add_osm_feature (qry, key = "highway")
+    qry <- osmdata::add_osm_feature (qry, key = "name", value = name,
                                  key_exact = FALSE, value_exact = FALSE,
                                  match_case = FALSE)
 
