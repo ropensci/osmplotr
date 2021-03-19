e496df8507410fe36bfda1952413216ddcf581fa
mpadge <mark.padgham@email.com>
2021-03-19 10:59:45 +0100

    lint print-osm-map.R

--- R/print-osm-map.R
+++ R/print-osm-map.R
@@ -1,6 +1,7 @@
 #' print_osm_map
 #'
-#' Prints an OSM map produced with \code{osmplotr} to a specified graphics device.
+#' Prints an OSM map produced with \code{osmplotr} to a specified graphics
+#' device.
 #'
 #' @param map The map to be printed; a \code{ggplot2} object produced by
 #' \code{osmplotr}.
@@ -18,17 +19,18 @@
 #'
 #' @examples
 #' bbox <- get_bbox (c (-0.13, 51.5, -0.11, 51.52))
-#' map <- osm_basemap (bbox = bbox, bg = 'gray20')
-#' map <- add_osm_objects (map, london$dat_BNR, col = 'gray40') 
+#' map <- osm_basemap (bbox = bbox, bg = "gray20")
+#' map <- add_osm_objects (map, london$dat_BNR, col = "gray40")
 #' print_osm_map (map, width = 7) # prints to screen device
 #' \dontrun{
-#' print_osm_map (map, file = 'map.png', width = 500, units = 'px')
+#' print_osm_map (map, file = "map.png", width = 500, units = "px")
 #' }
-print_osm_map <- function (map, width, height, filename,
-                           device, units = c('in', 'cm', 'mm', 'px'), dpi = 300)
-{
+print_osm_map <- function (map, width, height, filename, device,
+                           units = c("in", "cm", "mm", "px"),
+                           dpi = 300) {
+
     if (missing (map))
-        stop ('map must be supplied')
+        stop ("map must be supplied")
     if (missing (width) & missing (height))
         width <- 7
 
@@ -40,12 +42,12 @@ print_osm_map <- function (map, width, height, filename,
         width <- height * diff (xlims) / diff (ylims)
 
     units <- match.arg (units)
-    if (missing (device) & missing (filename))
-    {
+    if (missing (device) & missing (filename)) {
+
         dev.new (width = width, height = height)
         print (map)
-    } else
-    {
+    } else {
+
         dev <- get_graphics_device (device, filename, units, dpi = dpi)
         dev (file = filename, width = width, height = height)
         print (map)
@@ -56,19 +58,19 @@ print_osm_map <- function (map, width, height, filename,
 }
 
 # code from hadley/ggplot2::save
-get_graphics_device <- function (device, filename, units, dpi = 300)
-{
+get_graphics_device <- function (device, filename, units, dpi = 300) {
+
     devices <- list (
                      eps =  function (...)
                          grDevices::postscript (..., onefile = FALSE,
                                                 horizontal = FALSE,
-                                                paper = 'special'),
+                                                paper = "special"),
                      ps =  function (...)
                          grDevices::postscript (..., onefile = FALSE,
                                                 horizontal = FALSE,
-                                                paper = 'special'),
+                                                paper = "special"),
                      tex =  function (...) grDevices::pictex (...),
-                     pdf =  function (..., version = '1.4')
+                     pdf =  function (..., version = "1.4")
                          grDevices::pdf (..., version = version),
                      svg =  function (...) grDevices::svg (...),
                      png =  function (...)
