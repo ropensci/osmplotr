78ddc7dc1ccc5bd855aa7b3667b1111254c0ef7b
mpadge <mark.padgham@email.com>
2021-03-19 11:01:04 +0100

    lint test-fns.R

--- R/test-fns.R
+++ R/test-fns.R
@@ -1,37 +1,37 @@
 # ---------  lengths of vectors
-test_len1 <- function (a, txt)
-{
-    if (length (a) > 1)
-    {
-        warning (paste ('Only the first element of', txt, 'will be used'))
+test_len1 <- function (a, txt) {
+
+    if (length (a) > 1) {
+
+        warning (paste ("Only the first element of", txt, "will be used"))
         a <- a [1]
     }
     return (a)
 }
 
-test_len2 <- function (a, txt)
-{
-    if (length (a) > 2)
-    {
-        warning (paste ('Only the first two elements of', txt, 'will be used'))
+test_len2 <- function (a, txt) {
+
+    if (length (a) > 2) {
+
+        warning (paste ("Only the first two elements of", txt, "will be used"))
         a <- a [1:2]
     }
     return (a)
 }
 
 # ---------  object classes
-test_numeric <- function (a, txt, value)
-{
-    if (!is.numeric (a))
-    {
+test_numeric <- function (a, txt, value) {
+
+    if (!is.numeric (a)) {
+
         if (length (value) == 1)
-            vstr <- 'value'
+            vstr <- "value"
         else
-            vstr <- 'values'
-        w <- simpleWarning (paste (txt, 'must be numeric; using default', vstr))
+            vstr <- "values"
+        w <- simpleWarning (paste (txt, "must be numeric; using default", vstr))
         a <- tryCatch(as.numeric (a), warning = function(c) w)
-        if (is (a, "warning"))
-        {
+        if (is (a, "warning")) {
+
             warning (a)
             a <- value
         }
@@ -39,13 +39,13 @@ test_numeric <- function (a, txt, value)
     return (a)
 }
 
-test_logical <- function (a, txt, value)
-{
-    if (!is.logical (a))
-    {
-        w <- simpleWarning (paste (txt, 'must be logical; using default'))
-        if (is.na (as.logical (a)))
-        {
+test_logical <- function (a, txt, value) {
+
+    if (!is.logical (a)) {
+
+        w <- simpleWarning (paste (txt, "must be logical; using default"))
+        if (is.na (as.logical (a))) {
+
             warning (w)
             a <- value
         }
@@ -54,26 +54,26 @@ test_logical <- function (a, txt, value)
 }
 
 # ---------  object values
-test_pos <- function (a, txt, value)
-{
-    if (a < 0)
-    {
-        warning (paste (txt, 'must be positive; using default value'))
+test_pos <- function (a, txt, value) {
+
+    if (a < 0) {
+
+        warning (paste (txt, "must be positive; using default value"))
         a <- 3
     }
     return (a)
 }
 
-test_range <- function (a, txt, rng, value)
-{
-    if (any (a < rng [1]) | any (a > rng [2]))
-    {
+test_range <- function (a, txt, rng, value) {
+
+    if (any (a < rng [1]) | any (a > rng [2])) {
+
         if (length (value) == 1)
-            vstr <- 'value'
+            vstr <- "value"
         else
-            vstr <- 'values'
-        warning (paste0 (txt, ' not in [', rng [1], ',', rng [2],
-                         ']; using default ', vstr))
+            vstr <- "values"
+        warning (paste0 (txt, " not in [", rng [1], ",", rng [2],
+                         "]; using default ", vstr))
         a <- value
     }
     return (a)
