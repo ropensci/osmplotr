88bb11ac999a967617988c1b800aeeb717d4ec90
mpadge <mark.padgham@email.com>
2021-03-19 11:02:01 +0100

    lint R/zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
-.onAttach <- function(libname, pkgname) {
-    msg <- paste0 ('Data (c) OpenStreetMap contributors, ',
-                   'ODbL 1.0. http://www.openstreetmap.org/copyright')
+.onAttach <- function(libname, pkgname) {           # nolint
+    msg <- paste0 ("Data (c) OpenStreetMap contributors, ",
+                   "ODbL 1.0. http://www.openstreetmap.org/copyright")
     packageStartupMessage(msg)
 }
 
-.onLoad <- function(libname, pkgname) {
-    if (!requireNamespace ("sf"))
-    {
+.onLoad <- function(libname, pkgname) {         # nolint
+    if (!requireNamespace ("sf")) {
+
         packageStartupMessage ("NOTE: 'sf' package must be loaded for 'sf' ",
                                "methods to work")
     }
