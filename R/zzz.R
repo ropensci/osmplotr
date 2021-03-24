aedf28f253f03a37dc9e62535b8c03fcebbd9688
mpadge <mark.padgham@email.com>
2021-03-24 09:56:44 +0100

    rm .onLoad hook

--- R/zzz.R
+++ R/zzz.R
@@ -3,11 +3,3 @@
                    "ODbL 1.0. http://www.openstreetmap.org/copyright")
     packageStartupMessage(msg)
 }
-
-.onLoad <- function(libname, pkgname) {         # nolint
-    if (!requireNamespace ("sf")) {
-
-        packageStartupMessage ("NOTE: 'sf' package must be loaded for 'sf' ",
-                               "methods to work")
-    }
-}
