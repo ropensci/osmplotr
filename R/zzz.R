0f9df71f1d1e97070d404a614c032d14110e80a1
mpadge <mark.padgham@email.com>
2021-03-24 09:59:56 +0100

    reinstate .onLoad without the requireNamespace call

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,8 @@
                    "ODbL 1.0. http://www.openstreetmap.org/copyright")
     packageStartupMessage(msg)
 }
+
+.onLoad <- function(libname, pkgname) {         # nolint
+    packageStartupMessage ("NOTE: The 'sf' package should be loaded to ",
+                           "ensure that 'sf' methods work as expected.")
+}
