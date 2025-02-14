3bce1c3dc473ad444b6f8bddbca51d2a9c445013
mpadge <mark.padgham@email.com>
2025-02-14 10:35:10 +0100

    improve 'test_all' conditions to match job not workflow name

--- tests/testthat/test-print-map.R
+++ tests/testthat/test-print-map.R
@@ -1,7 +1,5 @@
-context ("print-osm-map")
-
-test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
-    identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
+test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") ||
+    identical (Sys.getenv ("GITHUB_JOB"), "test-coverage"))
 
 test_that ("print_osm_map", {
 
