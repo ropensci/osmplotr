7087d3e95af520d696c34499ce120588497c40c8
mpadge <mark.padgham@email.com>
2025-10-07 16:21:57 +0200

    mock all test results for test-extract-objects

--- tests/testthat/test-extract-objects.R
+++ tests/testthat/test-extract-objects.R
@@ -29,50 +29,67 @@ skip_if (!curl::has_internet ())
 skip_if (!test_all)
 
 test_that ("invalid key", {
-    bbox <- get_bbox (c (-0.12, 51.51, -0.11, 51.52))
+    bbox <- get_bbox (c (-0.1265, 51.5145, -0.1263, 51.5147))
     expect_warning (
         suppressMessages (
-            extract_osm_objects (bbox = bbox, key = "aaa")
+            httptest2::with_mock_dir ("extract-invalid-key", {
+                extract_osm_objects (bbox = bbox, key = "aaa")
+            })
         ),
         "No valid data returned"
     )
 })
 
 test_that ("valid key", {
-    bbox <- get_bbox (c (-0.12, 51.518, -0.118, 51.52))
-    dat <- extract_osm_objects (bbox = bbox, key = "building")
+    bbox <- get_bbox (c (-0.1265, 51.5145, -0.1263, 51.5147))
+    dat <- httptest2::with_mock_dir ("extract-buildings", {
+        extract_osm_objects (bbox = bbox, key = "building")
+    })
     expect_is (dat, "sf")
-    dat <- extract_osm_objects (
-        bbox = bbox, key = "building",
-        sf = FALSE
+
+    expect_warning (
+        dat <- httptest2::with_mock_dir ("extract-buildings-sp", {
+            extract_osm_objects (
+                bbox = bbox, key = "building",
+                sf = FALSE
+            )
+        }),
+        "\\'osmdata\\_sp \\(\\)\\' is deprecated"
     )
     expect_is (dat, "SpatialPolygonsDataFrame")
 })
 
 test_that ("extra_pairs", {
-    key <- "route"
-    value <- "bicycle"
-    extra_pairs <- c ("name", "London Cycle Network")
-    bbox <- get_bbox (c (0, 51.5, 0.1, 51.6))
-    dat <- extract_osm_objects (
-        bbox = bbox, key = key,
-        value = value,
-        extra_pairs = extra_pairs
-    )
+    key <- "building"
+    value <- "yes"
+    extra_pairs <- c ("amenity", "place_of_worship")
+    bbox <- get_bbox (c (-0.1274, 51.5140, -0.1270, 51.5144))
+    dat <- httptest2::with_mock_dir ("extract-bldg-extra-pair", {
+        extract_osm_objects (
+            bbox = bbox, key = key,
+            value = value,
+            extra_pairs = extra_pairs
+        )
+    })
     expect_true (nrow (dat) > 0)
     expect_is (dat, "sf")
 })
 
 test_that ("sp objects", {
-    key <- "route"
-    value <- "bicycle"
-    extra_pairs <- c ("name", "London Cycle Network")
-    bbox <- get_bbox (c (0, 51.5, 0.1, 51.6))
-    dat <- extract_osm_objects (
-        bbox = bbox, key = key,
-        value = value,
-        extra_pairs = extra_pairs,
-        sf = FALSE
+    key <- "building"
+    value <- "yes"
+    extra_pairs <- c ("amenity", "place_of_worship")
+    bbox <- get_bbox (c (-0.1274, 51.5140, -0.1270, 51.5144))
+    expect_warning (
+        dat <- httptest2::with_mock_dir ("extract-bldg-extra-pair-sp", {
+            extract_osm_objects (
+                bbox = bbox, key = key,
+                value = value,
+                extra_pairs = extra_pairs,
+                sf = FALSE
+            )
+        }),
+        "\\'osmdata\\_sp \\(\\)\\' is deprecated"
     )
     expect_true (nrow (dat) > 0)
     expect_is (dat, "Spatial")
